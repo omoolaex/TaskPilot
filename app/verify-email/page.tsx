dd9f126f8bcc1aff7dbaa3587e99c2ad87b421cb
Omoola003 <owolabigbolahan003@gmail.com>
2025-08-09 12:49:40 +0100

    parse 2 fix

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,6 +1,11 @@
 // app/verify-email/page.tsx
 import VerifyEmailClient from "./VerifyEmailClient";
+import { Suspense } from "react";
 
 export default function VerifyEmailPage() {
-  return <VerifyEmailClient />;
+  return (
+  <Suspense fallback={<p>Loading...</p>}>
+    <VerifyEmailClient />;
+  </Suspense>
+  );
 }
